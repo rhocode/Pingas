c325e8d829575838636dabe2ae7667901a61d10f
Adam <adam@thinkaliker.com>
2022-02-04 23:32:46 -0800

    graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+
+const data = [{ name: 'Page A', success: 1, failure: 0 }, { name: 'Page B', success: 0, failure: 1 }, { name: 'Page C', success: 1, failure: 1 },]
+
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="success" stroke="#00ff00" />
+          <Line type="monotone" dataKey="failure" stroke="#ff0000" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </LineChart>
       </header>
     </div>
   );
