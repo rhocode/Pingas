79ec0ae4cfe2f7141685556caf76ce778d3cd15a
Alex Fu <aafu@ucdavis.edu>
2022-02-04 23:33:41 -0800

    Rebased

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,64 @@
+import produce from "immer";
 
+const ping = async () => {
+    const host = "https://1.1.1.1/"
+    const controller = new AbortController()
 
-export const ping = () => {
-    return []
+    // 2 second timeout:
+    const timeoutId = setTimeout(() => controller.abort(), 2000)
+    const start = new Date().getTime();
+    const res = await fetch(host, { signal: controller.signal });
+    const end = new Date().getTime() - start;
+    clearTimeout(timeoutId);
+    if (res.ok) {
+        return end;
+    } else {
+        return -1
+    }
+}
+
+const createDateStr = (date: Date) => {
+   return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+'-'+date.getHours();
+}
+
+export const createInitialState = () => {
+    const now = new Date();
+    const stateMap = {} as Record<string, any>;
+    for (let i = 23; i >= 0; i--) {
+        now.setHours(now.getHours() - 1);
+        stateMap[createDateStr(now)] = {
+            success: 0,
+            failure: 0
+        };
+    }
+
+    return stateMap;
+}
+
+export const updateState = async (state: Record<string, any>) => {
+    const pingValue = await ping();
+
+    return produce(state, (draftState) => {
+        const key = createDateStr(new Date())
+
+        if (!draftState[key]) {
+            draftState[key] = {
+                success: 0,
+                failure: 0,
+                signals: []
+            }
+            const oldDate = new Date();
+            oldDate.setHours(oldDate.getHours() - 24);
+            delete draftState[createDateStr(oldDate)]
+        }
+
+        const obj = draftState[key];
+
+        if (pingValue >= 0) {
+            obj.success++;
+        } else {
+            obj.failure++;
+        }
+        obj.signals.push(pingValue >= 0 ? pingValue : -1)
+    })
 }
